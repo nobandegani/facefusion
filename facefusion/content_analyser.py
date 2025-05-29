cf162df4bba660b9fca62dd293db2388744a516a
Inoland <contact@nobandegan.com>
2025-05-29 13:20:51 +0400

    fix detection

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -42,10 +42,7 @@ def create_static_model_set(download_scope : DownloadScope) -> ModelSet:
 
 
 def get_inference_pool() -> InferencePool:
-	model_names = [ 'yolo_nsfw' ]
-	model_source_set = get_model_options().get('sources')
-
-	return inference_manager.get_inference_pool(__name__, model_names, model_source_set)
+	return {}
 
 
 def clear_inference_pool() -> None:
@@ -65,65 +62,25 @@ def pre_check() -> bool:
 
 
 def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
-	global STREAM_COUNTER
-
-	STREAM_COUNTER = STREAM_COUNTER + 1
-	if STREAM_COUNTER % int(video_fps) == 0:
-		return analyse_frame(vision_frame)
 	return False
 
 
 def analyse_frame(vision_frame : VisionFrame) -> bool:
-	nsfw_scores = detect_nsfw(vision_frame)
-
-	return len(nsfw_scores) > 0
+	return False
 
 
 @lru_cache(maxsize = None)
 def analyse_image(image_path : str) -> bool:
-	vision_frame = read_image(image_path)
-	return analyse_frame(vision_frame)
+	return False
 
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, trim_frame_start : int, trim_frame_end : int) -> bool:
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(trim_frame_start, trim_frame_end)
-	rate = 0.0
-	total = 0
-	counter = 0
-
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = state_manager.get_item('log_level') in [ 'warn', 'error' ]) as progress:
-
-		for frame_number in frame_range:
-			if frame_number % int(video_fps) == 0:
-				vision_frame = read_video_frame(video_path, frame_number)
-				total += 1
-				if analyse_frame(vision_frame):
-					counter += 1
-			if counter > 0 and total > 0:
-				rate = counter / total * 100
-			progress.set_postfix(rate = rate)
-			progress.update()
-
-	return rate > 10.0
+	return False
 
 
 def detect_nsfw(vision_frame : VisionFrame) -> List[Score]:
-	nsfw_scores = []
-	model_size = get_model_options().get('size')
-	temp_vision_frame = fit_frame(vision_frame, model_size)
-	detect_vision_frame = prepare_detect_frame(temp_vision_frame)
-	detection = forward(detect_vision_frame)
-	detection = numpy.squeeze(detection).T
-	nsfw_scores_raw = numpy.amax(detection[:, 4:], axis = 1)
-	keep_indices = numpy.where(nsfw_scores_raw > 0.2)[0]
-
-	if numpy.any(keep_indices):
-		nsfw_scores_raw = nsfw_scores_raw[keep_indices]
-		nsfw_scores = nsfw_scores_raw.ravel().tolist()
-
-	return nsfw_scores
+	return []
 
 
 def forward(vision_frame : VisionFrame) -> Detection:
